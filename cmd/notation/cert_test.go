77d7f19e926be5b4d925355ccdcd8877e9fc7e15
Patrick Zheng <patrickzheng@microsoft.com>
2022-09-13 14:58:06 +0800

    added cert_test.go for trust store
    Signed-off-by: Patrick Zheng <patrickzheng@microsoft.com>

--- cmd/notation/cert_test.go
+++ cmd/notation/cert_test.go
@@ -1,67 +1,117 @@
 package main
 
-// import (
-// 	"fmt"
-// 	"reflect"
-// 	"testing"
-// )
+import (
+	"reflect"
+	"testing"
+)
 
-// func TestCertAddCommand_BasicArgs(t *testing.T) {
-// 	opts := &certAddOpts{}
-// 	cmd := certAddCommand(opts)
-// 	expected := &certAddOpts{
-// 		path: "path",
-// 		name: "cert",
-// 	}
-// 	if err := cmd.ParseFlags([]string{
-// 		expected.path,
-// 		"-n", expected.name}); err != nil {
-// 		t.Fatalf("Parse Flag failed: %v", err)
-// 	}
-// 	if err := cmd.Args(cmd, cmd.Flags().Args()); err != nil {
-// 		t.Fatalf("Parse Args failed: %v", err)
-// 	}
-// 	if *expected != *opts {
-// 		t.Fatalf("Expect cert add opts: %v, got: %v", expected, opts)
-// 	}
-// }
+func TestCertAddCommand_BasicArgs(t *testing.T) {
+	opts := &certAddOpts{}
+	cmd := certAddCommand(opts)
+	expected := &certAddOpts{
+		storeType:  "ca",
+		namedStore: "test1",
+		path:       []string{"cert.pem"},
+	}
+	if err := cmd.ParseFlags([]string{
+		"cert.pem",
+		"-t", "ca", "-s", "test1"}); err != nil {
+		t.Fatalf("Parse Flag failed: %v", err)
+	}
+	if err := cmd.Args(cmd, cmd.Flags().Args()); err != nil {
+		t.Fatalf("Parse Args failed: %v", err)
+	}
+	if !reflect.DeepEqual(*expected, *opts) {
+		t.Fatalf("Expect cert add opts: %v, got: %v", expected, opts)
+	}
+}
 
-// func TestCertAddCommand_MissingArgs(t *testing.T) {
-// 	cmd := certAddCommand(nil)
-// 	if err := cmd.ParseFlags([]string{}); err != nil {
-// 		t.Fatalf("Parse Flag failed: %v", err)
-// 	}
-// 	if err := cmd.Args(cmd, cmd.Flags().Args()); err == nil {
-// 		t.Fatal("Parse Args expected error, but ok")
-// 	}
-// }
+func TestCertAddCommand_MissingArgs(t *testing.T) {
+	cmd := certAddCommand(nil)
+	if err := cmd.ParseFlags([]string{}); err != nil {
+		t.Fatalf("Parse Flag failed: %v", err)
+	}
+	if err := cmd.Args(cmd, cmd.Flags().Args()); err == nil {
+		t.Fatal("Parse Args expected error, but ok")
+	}
+}
 
-// func TestCertRemoveCommand_BasicArgs(t *testing.T) {
-// 	opts := &certRemoveOpts{}
-// 	cmd := certRemoveCommand(opts)
-// 	expected := &certRemoveOpts{
-// 		names: []string{"cert0", "cert1", "cert2"},
-// 	}
-// 	if err := cmd.ParseFlags(expected.names); err != nil {
-// 		t.Fatalf("Parse Flag failed: %v", err)
-// 	}
-// 	if err := cmd.Args(cmd, cmd.Flags().Args()); err != nil {
-// 		t.Fatalf("Parse Args failed: %v", err)
-// 	}
-// 	if !reflect.DeepEqual(*expected, *opts) {
-// 		t.Fatalf("Expect cert remove opts: %v, got: %v", expected, opts)
-// 	}
-// }
+func TestCertListCommand(t *testing.T) {
+	opts := &certListOpts{}
+	cmd := certListCommand(opts)
+	expected := &certListOpts{
+		storeType:  "ca",
+		namedStore: "test1",
+	}
+	if err := cmd.ParseFlags([]string{
+		"-t", "ca", "-s", "test1"}); err != nil {
+		t.Fatalf("Parse Flag failed: %v", err)
+	}
+	if !reflect.DeepEqual(*expected, *opts) {
+		t.Fatalf("Expect cert add opts: %v, got: %v", expected, opts)
+	}
+}
 
-// func TestCertRemoveCommand_MissingArgs(t *testing.T) {
-// 	cmd := certRemoveCommand(nil)
-// 	if err := cmd.ParseFlags([]string{}); err != nil {
-// 		t.Fatalf("Parse Flag failed: %v", err)
-// 	}
-// 	if err := cmd.Args(cmd, cmd.Flags().Args()); err == nil {
-// 		t.Fatal("Parse Args expected error, but ok")
-// 	}
-// }
+func TestCertShowCommand_BasicArgs(t *testing.T) {
+	opts := &certShowOpts{}
+	cmd := certShowCommand(opts)
+	expected := &certShowOpts{
+		storeType:  "ca",
+		namedStore: "test1",
+		cert:       "cert.pem",
+	}
+	if err := cmd.ParseFlags([]string{
+		"cert.pem",
+		"-t", "ca", "-s", "test1"}); err != nil {
+		t.Fatalf("Parse Flag failed: %v", err)
+	}
+	if err := cmd.Args(cmd, cmd.Flags().Args()); err != nil {
+		t.Fatalf("Parse Args failed: %v", err)
+	}
+	if !reflect.DeepEqual(*expected, *opts) {
+		t.Fatalf("Expect cert add opts: %v, got: %v", expected, opts)
+	}
+}
+
+func TestCertShowCommand_MissingArgs(t *testing.T) {
+	cmd := certShowCommand(nil)
+	if err := cmd.ParseFlags([]string{}); err != nil {
+		t.Fatalf("Parse Flag failed: %v", err)
+	}
+	if err := cmd.Args(cmd, cmd.Flags().Args()); err == nil {
+		t.Fatal("Parse Args expected error, but ok")
+	}
+}
+
+func TestCertRemoveCommand_BasicArgs(t *testing.T) {
+	opts := &certRemoveOpts{}
+	cmd := certRemoveCommand(opts)
+	expected := &certRemoveOpts{
+		storeType:  "ca",
+		namedStore: "test1",
+		all:        true,
+	}
+	if err := cmd.ParseFlags([]string{
+		"-t", "ca", "-s", "test1", "-a"}); err != nil {
+		t.Fatalf("Parse Flag failed: %v", err)
+	}
+	if err := cmd.Args(cmd, cmd.Flags().Args()); err != nil {
+		t.Fatalf("Parse Args failed: %v", err)
+	}
+	if !reflect.DeepEqual(*expected, *opts) {
+		t.Fatalf("Expect cert remove opts: %v, got: %v", expected, opts)
+	}
+}
+
+func TestCertRemoveCommand_MissingArgs(t *testing.T) {
+	cmd := certRemoveCommand(nil)
+	if err := cmd.ParseFlags([]string{}); err != nil {
+		t.Fatalf("Parse Flag failed: %v", err)
+	}
+	if err := cmd.Args(cmd, cmd.Flags().Args()); err == nil {
+		t.Fatal("Parse Args expected error, but ok")
+	}
+}
 
 // func TestCertGenerateCommand(t *testing.T) {
 // 	opts := &certGenerateTestOpts{}
