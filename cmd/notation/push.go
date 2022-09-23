2a944d32baf951018914549dd32a6e764dec7c0b
zaihaoyin <30316518+chloeyin@users.noreply.github.com>
2022-09-23 15:44:06 +0800

    refactor:support inspecting envelope (#349)
    Co-authored-by: zaihaoyin <zaihaoyin@microsoft.com>

--- cmd/notation/push.go
+++ cmd/notation/push.go
@@ -9,6 +9,7 @@ import (
 	"github.com/notaryproject/notation-go"
 	"github.com/notaryproject/notation-go/dir"
 	"github.com/notaryproject/notation-go/signature"
+	"github.com/notaryproject/notation/internal/envelope"
 	"github.com/notaryproject/notation/pkg/cache"
 	"github.com/spf13/cobra"
 )
@@ -72,7 +73,7 @@ func runPush(command *cobra.Command, opts *pushOpts) error {
 		}
 		// pass in nonempty annotations if needed
 		// TODO: understand media type in a better way
-		sigMediaType, err := signature.GuessSignatureEnvelopeFormat(sig)
+		sigMediaType, err := envelope.SpeculateSignatureEnvelopeFormat(sig)
 		if err != nil {
 			return err
 		}
