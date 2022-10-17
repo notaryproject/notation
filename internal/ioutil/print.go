6ad56081de84d5e1b3aa09d0936449e11218972c
Binbin Li <libinbin@microsoft.com>
2022-10-17 05:52:58 +0000

    refactor: refactor result format
    Signed-off-by: Binbin Li <libinbin@microsoft.com>

--- internal/ioutil/print.go
+++ internal/ioutil/print.go
@@ -59,24 +59,18 @@ func PrintVerificationResults(w io.Writer, v []*verification.SignatureVerificati
 
 	if resultErr == nil {
 		fmt.Fprintf(tw, "Signature verification succeeded for %s\n", digest)
+		// TODO: print out failed validations as warnings.
 		return nil
 	}
 
 	fmt.Fprintf(tw, "ERROR: %s\n\n", resultErr.Error())
-	printOutcomes(tw, v)
+	printOutcomes(tw, v, digest)
 
 	return tw.Flush()
 }
 
-func printOutcomes(tw *tabwriter.Writer, outcomes []*verification.SignatureVerificationOutcome) {
-	if len(outcomes) == 1 {
-		fmt.Println("1 signature failed verification, error is listed as below:")
-	} else {
-		fmt.Printf("%d signatures failed verification, errors are listed as below:\n", len(outcomes))
-	}
+func printOutcomes(tw *tabwriter.Writer, outcomes []*verification.SignatureVerificationOutcome, digest string) {
+	fmt.Printf("Signature verification failed for all the %d signatures associated with digest: %s\n", len(outcomes), digest)
 
-	for _, outcome := range outcomes {
-		// TODO: print out the signature digest once the outcome contains it.
-		fmt.Printf("%s\n\n", outcome.Error.Error())
-	}
+	// TODO: print out detailed errors in debug mode.
 }
