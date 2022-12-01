b6ca9f1f0d976db53aec58cc398d0f2d61b7e447
Patrick Zheng <patrickzheng@microsoft.com>
2022-12-01 17:02:39 +0800

    updated per code review
    Signed-off-by: Patrick Zheng <patrickzheng@microsoft.com>

--- cmd/notation/plugin.go
+++ cmd/notation/plugin.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"text/tabwriter"
 
 	"github.com/notaryproject/notation-go/dir"
 	"github.com/notaryproject/notation-go/plugin"
-	"github.com/notaryproject/notation/internal/ioutil"
+	"github.com/notaryproject/notation-go/plugin/proto"
 	"github.com/spf13/cobra"
 )
 
@@ -30,17 +32,28 @@ func pluginListCommand() *cobra.Command {
 }
 
 func listPlugins(command *cobra.Command) error {
+	var err error
+	var pl plugin.Plugin
 	mgr := plugin.NewCLIManager(dir.PluginFS())
 	pluginNames, err := mgr.List(command.Context())
 	if err != nil {
 		return err
 	}
-	var plugins []plugin.Plugin
-	var errors []error
+
+	tw := tabwriter.NewWriter(os.Stdout, 0, 0, 3, ' ', 0)
+	fmt.Fprintln(tw, "NAME\tDESCRIPTION\tVERSION\tCAPABILITIES\tERROR\t")
 	for _, n := range pluginNames {
-		pl, err := mgr.Get(command.Context(), n)
-		errors = append(errors, err)
-		plugins = append(plugins, pl)
+		pl, err = mgr.Get(command.Context(), n)
+		metaData := &proto.GetMetadataResponse{}
+		if pl != nil {
+			req := &proto.GetMetadataRequest{}
+			resp, err := pl.GetMetadata(command.Context(), req)
+			if err == nil {
+				metaData = resp
+			}
+		}
+		fmt.Fprintf(tw, "%s\t%s\t%s\t%v\t%v\t\n",
+			n, metaData.Description, metaData.Version, metaData.Capabilities, err)
 	}
-	return ioutil.PrintPlugins(command.Context(), os.Stdout, pluginNames, plugins, errors)
+	return tw.Flush()
 }
