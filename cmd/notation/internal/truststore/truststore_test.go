6962d3115aa4d41fc3d3de3c9fca3daac0df95a8
Patrick Zheng <patrickzheng@microsoft.com>
2022-12-19 18:59:48 +0800

    fixed unit test
    Signed-off-by: Patrick Zheng <patrickzheng@microsoft.com>

--- cmd/notation/internal/truststore/truststore_test.go
+++ cmd/notation/internal/truststore/truststore_test.go
@@ -10,7 +10,7 @@ func TestEmptyCertFile(t *testing.T) {
 	path := filepath.FromSlash("../../../../internal/testdata/Empty.txt")
 	expectedErr := errors.New("no valid certificate found in the empty file")
 	err := AddCert(path, "ca", "test", false)
-	if err == nil || err.Error() != "no valid certificate found in the empty file" {
+	if err == nil || err.Error() != "no valid certificate found in the file" {
 		t.Fatalf("expected err: %v, got: %v", expectedErr, err)
 	}
 }
