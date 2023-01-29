8dd8df13aba9300a09aa3df3719255b34de9c001
Junjie Gao <junjiegao@microsoft.com>
2023-01-29 17:35:21 +0800

    test: e2e sign/verify/trustpolicy test cases
    Signed-off-by: Junjie Gao <junjiegao@microsoft.com>

--- test/e2e/suite/command/sign.go
+++ test/e2e/suite/command/sign.go
@@ -1,29 +1,109 @@
 package command
 
 import (
+	"fmt"
+	"time"
+
 	. "github.com/notaryproject/notation/test/e2e/internal/notation"
 	"github.com/notaryproject/notation/test/e2e/internal/utils"
+	. "github.com/notaryproject/notation/test/e2e/suite/common"
 	. "github.com/onsi/ginkgo/v2"
 )
 
 var _ = Describe("notation sign", func() {
-	It("with JWS signature format", func() {
+	It("by digest", func() {
 		Host(BaseOptions(), func(notation *utils.ExecOpts, artifact *Artifact, vhost *utils.VirtualHost) {
-			notation.Exec("sign", artifact.ReferenceWithTag(), "--signature-format", "jws").
-				MatchKeyWords("Successfully signed")
+			notation.Exec("sign", artifact.ReferenceWithDigest()).
+				MatchKeyWords(SignSuccessfully)
 
-			OldNotation().Exec("verify", artifact.ReferenceWithTag()).
-				MatchKeyWords("Successfully verified")
+			OldNotation().WithDescription("verify by digest").
+				Exec("verify", artifact.ReferenceWithDigest()).
+				MatchKeyWords(VerifySuccessfully)
+
+			OldNotation().WithDescription("verify by tag").
+				Exec("verify", artifact.ReferenceWithTag()).
+				MatchKeyWords(VerifySuccessfully)
 		})
 	})
 
-	It("with COSE signature format", func() {
+	It("by digest with COSE format", func() {
 		Host(BaseOptions(), func(notation *utils.ExecOpts, artifact *Artifact, vhost *utils.VirtualHost) {
-			notation.Exec("sign", artifact.ReferenceWithTag(), "--signature-format", "cose").
-				MatchKeyWords("Successfully signed")
+			notation.Exec("sign", "--signature-format", "cose", artifact.ReferenceWithDigest()).
+				MatchKeyWords(SignSuccessfully)
+
+			OldNotation().WithDescription("verify by digest").
+				Exec("verify", artifact.ReferenceWithTag()).
+				MatchKeyWords(VerifySuccessfully)
+
+			OldNotation().WithDescription("verify by tag").
+				Exec("verify", artifact.ReferenceWithTag()).
+				MatchKeyWords(VerifySuccessfully)
+		})
+	})
+
+	It("by tag with JWS format", func() {
+		Host(BaseOptions(), func(notation *utils.ExecOpts, artifact *Artifact, vhost *utils.VirtualHost) {
+			notation.WithDescription("sign with JWS").
+				Exec("sign", artifact.ReferenceWithTag(), "--signature-format", "jws").
+				MatchKeyWords(SignSuccessfully)
+
+			OldNotation().WithDescription("verify JWS signature").
+				Exec("verify", artifact.ReferenceWithTag()).
+				MatchKeyWords(VerifySuccessfully)
+		})
+	})
+
+	It("by tag with COSE signature format", func() {
+		Host(BaseOptions(), func(notation *utils.ExecOpts, artifact *Artifact, vhost *utils.VirtualHost) {
+			notation.WithDescription("sign with COSE").
+				Exec("sign", artifact.ReferenceWithTag(), "--signature-format", "cose").
+				MatchKeyWords(SignSuccessfully)
+
+			OldNotation().WithDescription("verify COSE signature").
+				Exec("verify", artifact.ReferenceWithTag()).
+				MatchKeyWords(VerifySuccessfully)
+		})
+	})
+
+	It("with specific key", func() {
+		Host(BaseOptions(), func(notation *utils.ExecOpts, artifact *Artifact, vhost *utils.VirtualHost) {
+			const keyName = "sKey"
+			notation.Exec("cert", "generate-test", keyName).
+				MatchKeyWords(fmt.Sprintf("notation/localkeys/%s.crt", keyName))
+
+			notation.Exec("sign", "--key", keyName, artifact.ReferenceWithDigest()).
+				MatchKeyWords(SignSuccessfully)
+
+			// copy the generated cert file and create the new trust policy for verify signature with generated new key.
+			OldNotation(AuthOption("", ""),
+				AddTrustStoreOption(keyName, vhost.AbsolutePath(NotationDirName, LocalKeysDirName, keyName+".crt")),
+				AddTrustPolicyOption("generate_test_trustpolicy.json"),
+			).Exec("verify", artifact.ReferenceWithTag()).
+				MatchKeyWords(VerifySuccessfully)
+		})
+	})
+
+	It("with expiry in 24h", func() {
+		Host(BaseOptions(), func(notation *utils.ExecOpts, artifact *Artifact, vhost *utils.VirtualHost) {
+			notation.Exec("sign", "--expiry", "24h", artifact.ReferenceWithDigest()).
+				MatchKeyWords(SignSuccessfully)
 
 			OldNotation().Exec("verify", artifact.ReferenceWithTag()).
-				MatchKeyWords("Successfully verified")
+				MatchKeyWords(VerifySuccessfully)
+		})
+	})
+
+	It("with expiry in 2s", func() {
+		Host(BaseOptions(), func(notation *utils.ExecOpts, artifact *Artifact, vhost *utils.VirtualHost) {
+			notation.Exec("sign", "--expiry", "2s", artifact.ReferenceWithDigest()).
+				MatchKeyWords(SignSuccessfully)
+
+			// sleep to wait for expiry
+			time.Sleep(2100 * time.Millisecond)
+
+			OldNotation().ExpectFailure().Exec("verify", artifact.ReferenceWithDigest(), "-v").
+				MatchErrKeyWords("expiry validation failed.").
+				MatchErrKeyWords("signature verification failed for all the signatures")
 		})
 	})
 })
---
+++ test/e2e/suite/trustpolicy/verification_level.go
@@ -0,0 +1,330 @@
+package trustpolicy
+
+import (
+	"path/filepath"
+
+	. "github.com/notaryproject/notation/test/e2e/internal/notation"
+	"github.com/notaryproject/notation/test/e2e/internal/utils"
+	. "github.com/notaryproject/notation/test/e2e/suite/common"
+	. "github.com/onsi/ginkgo/v2"
+)
+
+var _ = Describe("notation trust policy verification level test", func() {
+	It("strict level with expired signature", func() {
+		Host(BaseOptions(), func(notation *utils.ExecOpts, _ *Artifact, vhost *utils.VirtualHost) {
+			artifact := GenerateArtifact("e2e-expired-signature", "")
+
+			notation.ExpectFailure().Exec("verify", artifact.ReferenceWithDigest(), "-v").
+				MatchErrKeyWords("expiry validation failed.",
+					VerifyFailed)
+		})
+	})
+
+	It("strict level with expired authentic timestamp", func() {
+		Host(nil, func(notation *utils.ExecOpts, _ *Artifact, vhost *utils.VirtualHost) {
+			artifact := GenerateArtifact("e2e-with-expired-cert", "")
+
+			vhost.SetOption(AuthOption("", ""),
+				AddTrustPolicyOption("trustpolicy.json"),
+				AddTrustStoreOption("e2e", filepath.Join(NotationE2EConfigPath, "localkeys", "expired_e2e.crt")))
+
+			notation.ExpectFailure().Exec("verify", artifact.ReferenceWithDigest(), "-v").
+				MatchErrKeyWords("authenticTimestamp validation failed",
+					VerifyFailed)
+		})
+	})
+
+	It("strict level with invalid authenticity", func() {
+		Host(nil, func(notation *utils.ExecOpts, _ *Artifact, vhost *utils.VirtualHost) {
+			vhost.SetOption(AuthOption("", ""),
+				AddTrustPolicyOption("trustpolicy.json"),
+				AddTrustStoreOption("e2e", filepath.Join(NotationE2ELocalKeysDir, "new_e2e.crt")))
+
+			// the artifact signed with a different cert from the cert in
+			// trust store.
+			artifact := GenerateArtifact("e2e-valid-signature", "")
+
+			notation.ExpectFailure().Exec("verify", artifact.ReferenceWithDigest(), "-v").
+				MatchErrKeyWords("authenticity validation failed",
+					VerifyFailed)
+		})
+	})
+
+	It("strict level with invalid integrity", func() {
+		Host(BaseOptions(), func(notation *utils.ExecOpts, _ *Artifact, vhost *utils.VirtualHost) {
+			artifact := GenerateArtifact("e2e-invalid-signature", "")
+
+			notation.ExpectFailure().Exec("verify", artifact.ReferenceWithDigest(), "-v").
+				MatchErrKeyWords("integrity validation failed",
+					VerifyFailed)
+		})
+	})
+
+	It("permissive level with expired signature", func() {
+		Host(BaseOptions(), func(notation *utils.ExecOpts, _ *Artifact, vhost *utils.VirtualHost) {
+			vhost.SetOption(AddTrustPolicyOption("permissive_trustpolicy.json"))
+
+			artifact := GenerateArtifact("e2e-expired-signature", "")
+
+			notation.Exec("verify", artifact.ReferenceWithDigest(), "-v").
+				MatchKeyWords("digital signature has expired",
+					"expiry was set to \"log\"",
+					VerifySuccessfully)
+		})
+	})
+
+	It("permissive level with expired authentic timestamp", func() {
+		Host(nil, func(notation *utils.ExecOpts, _ *Artifact, vhost *utils.VirtualHost) {
+			artifact := GenerateArtifact("e2e-with-expired-cert", "")
+
+			vhost.SetOption(AuthOption("", ""),
+				AddTrustPolicyOption("permissive_trustpolicy.json"),
+				AddTrustStoreOption("e2e", filepath.Join(NotationE2EConfigPath, "localkeys", "expired_e2e.crt")))
+
+			notation.Exec("verify", artifact.ReferenceWithDigest(), "-v").
+				MatchKeyWords("Warning: authenticTimestamp was set to \"log\"",
+					"error: certificate \"O=Internet Widgits Pty Ltd,ST=Some-State,C=AU\" is not valid anymore, it was expired",
+					VerifySuccessfully)
+		})
+	})
+
+	It("permissive level with invalid authenticity", func() {
+		Host(nil, func(notation *utils.ExecOpts, _ *Artifact, vhost *utils.VirtualHost) {
+			vhost.SetOption(AuthOption("", ""),
+				AddTrustPolicyOption("permissive_trustpolicy.json"),
+				AddTrustStoreOption("e2e", filepath.Join(NotationE2ELocalKeysDir, "new_e2e.crt")))
+
+			// the artifact signed with a different cert from the cert in
+			// trust store.
+			artifact := GenerateArtifact("e2e-valid-signature", "")
+
+			notation.ExpectFailure().Exec("verify", artifact.ReferenceWithDigest(), "-v").
+				MatchErrKeyWords("authenticity validation failed",
+					VerifyFailed)
+		})
+	})
+
+	It("permissive level with invalid integrity", func() {
+		Host(BaseOptions(), func(notation *utils.ExecOpts, _ *Artifact, vhost *utils.VirtualHost) {
+			vhost.SetOption(AddTrustPolicyOption("permissive_trustpolicy.json"))
+
+			artifact := GenerateArtifact("e2e-invalid-signature", "")
+
+			notation.ExpectFailure().Exec("verify", artifact.ReferenceWithDigest(), "-v").
+				MatchErrKeyWords("integrity validation failed",
+					VerifyFailed)
+		})
+	})
+
+	It("audit level with expired signature", func() {
+		Host(BaseOptions(), func(notation *utils.ExecOpts, _ *Artifact, vhost *utils.VirtualHost) {
+			vhost.SetOption(AddTrustPolicyOption("audit_trustpolicy.json"))
+
+			artifact := GenerateArtifact("e2e-expired-signature", "")
+
+			notation.Exec("verify", artifact.ReferenceWithDigest(), "-v").
+				MatchKeyWords("digital signature has expired",
+					"expiry was set to \"log\"",
+					VerifySuccessfully)
+		})
+	})
+
+	It("audit level with expired authentic timestamp", func() {
+		Host(nil, func(notation *utils.ExecOpts, _ *Artifact, vhost *utils.VirtualHost) {
+			artifact := GenerateArtifact("e2e-with-expired-cert", "")
+
+			vhost.SetOption(AuthOption("", ""),
+				AddTrustPolicyOption("audit_trustpolicy.json"),
+				AddTrustStoreOption("e2e", filepath.Join(NotationE2EConfigPath, "localkeys", "expired_e2e.crt")))
+
+			notation.Exec("verify", artifact.ReferenceWithDigest(), "-v").
+				MatchKeyWords("Warning: authenticTimestamp was set to \"log\"",
+					"error: certificate \"O=Internet Widgits Pty Ltd,ST=Some-State,C=AU\" is not valid anymore, it was expired",
+					VerifySuccessfully)
+		})
+	})
+
+	It("audit level with invalid authenticity", func() {
+		Host(nil, func(notation *utils.ExecOpts, _ *Artifact, vhost *utils.VirtualHost) {
+			vhost.SetOption(AuthOption("", ""),
+				AddTrustPolicyOption("audit_trustpolicy.json"),
+				AddTrustStoreOption("e2e", filepath.Join(NotationE2ELocalKeysDir, "new_e2e.crt")))
+
+			// the artifact signed with a different cert from the cert in
+			// trust store.
+			artifact := GenerateArtifact("e2e-valid-signature", "")
+
+			notation.Exec("verify", artifact.ReferenceWithDigest(), "-v").
+				MatchKeyWords("Warning: authenticity was set to \"log\"",
+					"signature is not produced by a trusted signer",
+					VerifySuccessfully)
+		})
+	})
+
+	It("audit level with invalid integrity", func() {
+		Host(BaseOptions(), func(notation *utils.ExecOpts, _ *Artifact, vhost *utils.VirtualHost) {
+			vhost.SetOption(AddTrustPolicyOption("audit_trustpolicy.json"))
+
+			artifact := GenerateArtifact("e2e-invalid-signature", "")
+
+			notation.ExpectFailure().Exec("verify", artifact.ReferenceWithDigest(), "-v").
+				MatchErrKeyWords("integrity validation failed",
+					VerifyFailed)
+		})
+	})
+
+	It("skip level with invalid integrity", func() {
+		Host(BaseOptions(), func(notation *utils.ExecOpts, _ *Artifact, vhost *utils.VirtualHost) {
+			vhost.SetOption(AddTrustPolicyOption("skip_trustpolicy.json"))
+
+			artifact := GenerateArtifact("e2e-invalid-signature", "")
+
+			notation.Exec("verify", artifact.ReferenceWithDigest(), "-v").
+				MatchKeyWords("Trust policy is configured to skip signature verification")
+		})
+	})
+
+	It("strict level with Expiry overridden as log level", func() {
+		Host(BaseOptions(), func(notation *utils.ExecOpts, _ *Artifact, vhost *utils.VirtualHost) {
+			vhost.SetOption(AddTrustPolicyOption("override_strict_trustpolicy.json"))
+
+			artifact := GenerateArtifact("e2e-expired-signature", "")
+
+			notation.Exec("verify", artifact.ReferenceWithDigest(), "-v").
+				MatchKeyWords("digital signature has expired",
+					"expiry was set to \"log\"",
+					VerifySuccessfully)
+		})
+	})
+
+	It("strict level with Authentic timestamp overridden as log level", func() {
+		Host(nil, func(notation *utils.ExecOpts, _ *Artifact, vhost *utils.VirtualHost) {
+			artifact := GenerateArtifact("e2e-with-expired-cert", "")
+
+			vhost.SetOption(AuthOption("", ""),
+				AddTrustPolicyOption("override_strict_trustpolicy.json"),
+				AddTrustStoreOption("e2e", filepath.Join(NotationE2EConfigPath, "localkeys", "expired_e2e.crt")))
+
+			notation.Exec("verify", artifact.ReferenceWithDigest(), "-v").
+				MatchKeyWords("Warning: authenticTimestamp was set to \"log\"",
+					"error: certificate \"O=Internet Widgits Pty Ltd,ST=Some-State,C=AU\" is not valid anymore, it was expired",
+					VerifySuccessfully)
+		})
+	})
+
+	It("strict level with Authenticity overridden as log level", func() {
+		Host(nil, func(notation *utils.ExecOpts, _ *Artifact, vhost *utils.VirtualHost) {
+			vhost.SetOption(AuthOption("", ""),
+				AddTrustPolicyOption("override_strict_trustpolicy.json"),
+				AddTrustStoreOption("e2e", filepath.Join(NotationE2ELocalKeysDir, "new_e2e.crt")))
+			// the artifact signed with a different cert from the cert in
+			// trust store.
+			artifact := GenerateArtifact("e2e-valid-signature", "")
+
+			notation.Exec("verify", artifact.ReferenceWithDigest(), "-v").
+				MatchKeyWords("Warning: authenticity was set to \"log\"",
+					"signature is not produced by a trusted signer",
+					VerifySuccessfully)
+		})
+	})
+
+	It("permissive level with Expiry overridden as enforce level", func() {
+		Host(BaseOptions(), func(notation *utils.ExecOpts, _ *Artifact, vhost *utils.VirtualHost) {
+			vhost.SetOption(AddTrustPolicyOption("override_permissive_trustpolicy.json"))
+
+			artifact := GenerateArtifact("e2e-expired-signature", "")
+
+			notation.ExpectFailure().Exec("verify", artifact.ReferenceWithDigest(), "-v").
+				MatchErrKeyWords("expiry validation failed.",
+					VerifyFailed)
+		})
+	})
+
+	It("permissive level with Authentic timestamp overridden as enforce level", func() {
+		Host(nil, func(notation *utils.ExecOpts, _ *Artifact, vhost *utils.VirtualHost) {
+			vhost.SetOption(AddTrustPolicyOption("override_permissive_trustpolicy.json"))
+
+			artifact := GenerateArtifact("e2e-with-expired-cert", "")
+
+			vhost.SetOption(AuthOption("", ""),
+				AddTrustPolicyOption("trustpolicy.json"),
+				AddTrustStoreOption("e2e", filepath.Join(NotationE2EConfigPath, "localkeys", "expired_e2e.crt")))
+
+			notation.ExpectFailure().Exec("verify", artifact.ReferenceWithDigest(), "-v").
+				MatchErrKeyWords("authenticTimestamp validation failed",
+					VerifyFailed)
+		})
+	})
+
+	It("permissive level with Authenticity overridden as log level", func() {
+		Host(nil, func(notation *utils.ExecOpts, _ *Artifact, vhost *utils.VirtualHost) {
+			vhost.SetOption(AuthOption("", ""),
+				AddTrustPolicyOption("override_permissive_trustpolicy.json"),
+				AddTrustStoreOption("e2e", filepath.Join(NotationE2ELocalKeysDir, "new_e2e.crt")))
+
+			artifact := GenerateArtifact("e2e-valid-signature", "")
+
+			notation.Exec("verify", artifact.ReferenceWithDigest(), "-v").
+				MatchKeyWords("Warning: authenticity was set to \"log\"",
+					"signature is not produced by a trusted signer",
+					VerifySuccessfully)
+		})
+	})
+
+	It("permissive level with Integrity overridden as log level", func() {
+		Host(nil, func(notation *utils.ExecOpts, _ *Artifact, vhost *utils.VirtualHost) {
+			vhost.SetOption(AuthOption("", ""),
+				AddTrustPolicyOption("override_integrity_for_permissive_trustpolicy.json"),
+				AddTrustStoreOption("e2e", filepath.Join(NotationE2ELocalKeysDir, "new_e2e.crt")))
+
+			artifact := GenerateArtifact("e2e-valid-signature", "")
+
+			notation.ExpectFailure().Exec("verify", artifact.ReferenceWithDigest(), "-v").
+				MatchErrKeyWords(`"integrity" verification can not be overridden in custom signature verification`)
+		})
+	})
+
+	It("audit level with Expiry overridden as enforce level", func() {
+		Host(BaseOptions(), func(notation *utils.ExecOpts, _ *Artifact, vhost *utils.VirtualHost) {
+			vhost.SetOption(AddTrustPolicyOption("override_audit_trustpolicy.json"))
+
+			artifact := GenerateArtifact("e2e-expired-signature", "")
+
+			notation.ExpectFailure().Exec("verify", artifact.ReferenceWithDigest(), "-v").
+				MatchErrKeyWords("expiry validation failed.",
+					VerifyFailed)
+		})
+	})
+
+	It("audit level with Authentic timestamp overridden as enforce level", func() {
+		Host(nil, func(notation *utils.ExecOpts, _ *Artifact, vhost *utils.VirtualHost) {
+			vhost.SetOption(AddTrustPolicyOption("override_audit_trustpolicy.json"))
+
+			artifact := GenerateArtifact("e2e-with-expired-cert", "")
+
+			vhost.SetOption(AuthOption("", ""),
+				AddTrustPolicyOption("trustpolicy.json"),
+				AddTrustStoreOption("e2e", filepath.Join(NotationE2EConfigPath, "localkeys", "expired_e2e.crt")))
+
+			notation.ExpectFailure().Exec("verify", artifact.ReferenceWithDigest(), "-v").
+				MatchErrKeyWords("authenticTimestamp validation failed",
+					VerifyFailed)
+		})
+	})
+
+	It("audit level with Authenticity overridden as enforce level", func() {
+		Host(nil, func(notation *utils.ExecOpts, _ *Artifact, vhost *utils.VirtualHost) {
+			vhost.SetOption(AuthOption("", ""),
+				AddTrustPolicyOption("override_audit_trustpolicy.json"),
+				AddTrustStoreOption("e2e", filepath.Join(NotationE2ELocalKeysDir, "new_e2e.crt")))
+
+			// the artifact signed with a different cert from the cert in
+			// trust store.
+			artifact := GenerateArtifact("e2e-valid-signature", "")
+
+			notation.ExpectFailure().Exec("verify", artifact.ReferenceWithDigest(), "-v").
+				MatchErrKeyWords("authenticity validation failed",
+					VerifyFailed)
+		})
+	})
+})
