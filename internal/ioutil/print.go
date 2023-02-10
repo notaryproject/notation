33c228132389d0b0f7b8c13aa25bf124f0d0b485
byronchien <byronc@ucla.edu>
2023-02-09 16:28:18 -0800

    feat: add support for json output for `notation verify` (#527)
    allows json output for `notation verify`. Fixes https://github.com/notaryproject/roadmap/issues/67 and https://github.com/notaryproject/notation/pull/498
    
    chienb@a07817b52895 notation % ./bin/notation verify $IMAGE --output json
    {
        "reference": "localhost:5000/net-monitor@sha256:5a07385af4e6b6af81b0ebfd435aedccdfa3507f0609c658209e1aba57159b2b",
        "userMetadata": {
            "foo": "bar"
        },
        "result": "Success"
    }
    
    Signed-off-by: Byron Chien <chienb@amazon.com>

--- internal/ioutil/print.go
+++ internal/ioutil/print.go
@@ -1,6 +1,7 @@
 package ioutil
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"text/tabwriter"
@@ -33,6 +34,7 @@ func PrintKeyMap(w io.Writer, target *string, v []config.KeySuite) error {
 	return tw.Flush()
 }
 
+// PrintMetadataMap prints a map to a given Writer as a table
 func PrintMetadataMap(w io.Writer, metadata map[string]string) error {
 	tw := newTabWriter(w)
 	fmt.Fprintln(tw, "\nKEY\tVALUE\t")
@@ -42,4 +44,16 @@ func PrintMetadataMap(w io.Writer, metadata map[string]string) error {
 	}
 
 	return tw.Flush()
+}
+
+// PrintObjectAsJSON takes an interface and prints it as an indented JSON string
+func PrintObjectAsJSON(i interface{}) error {
+	jsonBytes, err := json.MarshalIndent(i, "", "    ")
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(string(jsonBytes))
+
+	return nil
 }
