65b0a4ce993a344d9a629bc266ba4271e6dc366f
Patrick Zheng <patrickzheng@microsoft.com>
2023-04-17 15:08:14 +0800

    fix: fixing cert command (#627)
    This PR aims to resolve #622.
    It also adds MarkFlagRequired() to required flags in notation cert
    command.
    
    ---------
    
    Signed-off-by: Patrick Zheng <patrickzheng@microsoft.com>

--- cmd/notation/cert/add.go
+++ cmd/notation/cert/add.go
@@ -42,6 +42,8 @@ Example - Add a certificate to the "signingAuthority" type of a named store "wab
 	}
 	command.Flags().StringVarP(&opts.storeType, "type", "t", "", "specify trust store type, options: ca, signingAuthority")
 	command.Flags().StringVarP(&opts.namedStore, "store", "s", "", "specify named store")
+	command.MarkFlagRequired("type")
+	command.MarkFlagRequired("store")
 	return command
 }
 
