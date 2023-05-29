84aa94d93efb823c0eac0dc9346e36340302bf85
Junjie Gao <junjiegao@microsoft.com>
2023-05-29 19:59:18 +0800

    fix: resolve comment
    Signed-off-by: Junjie Gao <junjiegao@microsoft.com>

--- test/e2e/suite/command/list.go
+++ test/e2e/suite/command/list.go
@@ -52,14 +52,16 @@ var _ = Describe("notation list", func() {
 	})
 
 	It("all signatures of an oci-layout", func() {
-		HostWithOCILayout(BaseOptionsWithExperimental(), func(notation *utils.ExecOpts, ociLayout *OCILayout, vhost *utils.VirtualHost) {
-			notation.Exec("sign", "--oci-layout", ociLayout.ReferenceWithDigest()).
-				MatchKeyWords(SignSuccessfully)
+		HostWithOCILayout(BaseOptionsWithExperimental(), func(notation *utils.ExecOpts, _ *OCILayout, vhost *utils.VirtualHost) {
+			ociLayout, err := GenerateOCILayout("e2e-valid-signature")
+			if err != nil {
+				Fail(err.Error())
+			}
 
 			notation.Exec("list", "--oci-layout", ociLayout.ReferenceWithDigest()).
 				MatchKeyWords(
 					"└── application/vnd.cncf.notary.signature",
-					"└── sha256:",
+					"└── sha256:273243a7a64e9312761ca0aa8f43b6ba805e677a561558143b6e92981c487339",
 				)
 		})
 	})
