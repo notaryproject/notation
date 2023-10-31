1e173a4ed46c92324af1bfee683b1e1114c07b22
Patrick Zheng <patrickzheng@microsoft.com>
2023-10-31 14:47:29 +0800

    update
    Signed-off-by: Patrick Zheng <patrickzheng@microsoft.com>

--- cmd/notation/internal/plugin/plugin_test.go
+++ cmd/notation/internal/plugin/plugin_test.go
@@ -2,26 +2,12 @@ package plugin
 
 import (
 	"context"
-	"fmt"
-	"runtime"
 	"testing"
-
-	"github.com/notaryproject/notation-go/dir"
 )
 
 func TestCheckPluginExistence(t *testing.T) {
-	dir.UserConfigDir = "testdata"
-	fmt.Println(dir.PluginFS())
-	if runtime.GOOS == "windows" {
-		t.Skip("skipping test on Windows")
-	}
 	exist, err := CheckPluginExistence(context.Background(), "non-exist-plugin")
 	if exist || err != nil {
 		t.Fatalf("expected exist to be false with nil err, got: %v, %s", exist, err)
-	}
-
-	exist, err = CheckPluginExistence(context.Background(), "test-plugin")
-	if !exist || err != nil {
-		t.Fatalf("expected exist to be true with nil err, got: %v, %s", exist, err)
 	}
 }
