981cef3ae161c1da58e4e19d3a11b86d671df155
Patrick Zheng <patrickzheng@microsoft.com>
2023-11-07 16:19:54 +0800

    fixed unit tests
    Signed-off-by: Patrick Zheng <patrickzheng@microsoft.com>

--- cmd/notation/internal/plugin/plugin_test.go
+++ cmd/notation/internal/plugin/plugin_test.go
@@ -13,8 +13,8 @@ func TestCheckPluginExistence(t *testing.T) {
 }
 
 func TestValidateCheckSum(t *testing.T) {
-	expectedErrorMsg := "plugin checkSum does not match user input. User input is invalid, got e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
-	if err := ValidateCheckSum("./testdata/test", "invalid"); err == nil || err.Error() != expectedErrorMsg {
+	expectedErrorMsg := "plugin checksum does not match user input. Expecting abcd123"
+	if err := ValidateCheckSum("./testdata/test", "abcd123"); err == nil || err.Error() != expectedErrorMsg {
 		t.Fatalf("expected err %s, got %v", expectedErrorMsg, err)
 	}
 	if err := ValidateCheckSum("./testdata/test", "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"); err != nil {
@@ -27,26 +27,26 @@ func TestExtractPluginNameFromExecutableFileName(t *testing.T) {
 	if err != nil {
 		t.Fatalf("expected nil err, got %v", err)
 	}
-	if pluginName != "my-pluing" {
-		t.Fatalf("expected plugin name my-plugin, got %s", pluginName)
+	if pluginName != "my-plugin" {
+		t.Fatalf("expected plugin name my-plugin, but got %s", pluginName)
 	}
 
 	pluginName, err = ExtractPluginNameFromExecutableFileName("notation-my-plugin.exe")
 	if err != nil {
 		t.Fatalf("expected nil err, got %v", err)
 	}
-	if pluginName != "my-pluing" {
-		t.Fatalf("expected plugin name my-plugin, got %s", pluginName)
+	if pluginName != "my-plugin" {
+		t.Fatalf("expected plugin name my-plugin, but got %s", pluginName)
 	}
 
 	_, err = ExtractPluginNameFromExecutableFileName("myPlugin")
-	expectedErrorMsg := "invalid plugin executable file name. file name requires format notation-{plugin-name}, got myPlugin"
+	expectedErrorMsg := "invalid plugin executable file name. file name requires format notation-{plugin-name}, but got myPlugin"
 	if err == nil || err.Error() != expectedErrorMsg {
 		t.Fatalf("expected %s, got %v", expectedErrorMsg, err)
 	}
 
 	_, err = ExtractPluginNameFromExecutableFileName("my-plugin")
-	expectedErrorMsg = "invalid plugin executable file name. file name requires format notation-{plugin-name}, got my-plugin"
+	expectedErrorMsg = "invalid plugin executable file name. file name requires format notation-{plugin-name}, but got my-plugin"
 	if err == nil || err.Error() != expectedErrorMsg {
 		t.Fatalf("expected %s, got %v", expectedErrorMsg, err)
 	}
