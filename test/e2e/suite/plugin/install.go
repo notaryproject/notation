f82907921fc3240b20dce3023bc9da1402653dd2
Patrick Zheng <patrickzheng@microsoft.com>
2024-01-02 12:52:32 +0800

    fix e2e tests
    Signed-off-by: Patrick Zheng <patrickzheng@microsoft.com>

--- test/e2e/suite/plugin/install.go
+++ test/e2e/suite/plugin/install.go
@@ -62,14 +62,14 @@ var _ = Describe("notation plugin install", func() {
 
 	It("with content inside zip archive exceeds 256 MiB size limit", func() {
 		Host(nil, func(notation *utils.ExecOpts, _ *Artifact, vhost *utils.VirtualHost) {
-			notation.ExpectFailure().Exec("plugin", "install", "--file", NotationE2EMaliciousPluginArchivePath+"/largeFileZip.zip", "-v").
+			notation.ExpectFailure().Exec("plugin", "install", "--file", NotationE2EMaliciousPluginArchivePath+"/large_file_zip.zip", "-v").
 				MatchErrContent("Error: plugin installation failed: file reached the 256 MiB size limit\n")
 		})
 	})
 
 	It("with content inside tar.gz archive exceeds 256 MiB size limit", func() {
 		Host(nil, func(notation *utils.ExecOpts, _ *Artifact, vhost *utils.VirtualHost) {
-			notation.ExpectFailure().Exec("plugin", "install", "--file", NotationE2EMaliciousPluginArchivePath+"/largeFileTarGzip.tar.gz", "-v").
+			notation.ExpectFailure().Exec("plugin", "install", "--file", NotationE2EMaliciousPluginArchivePath+"/large_file_tarGz.tar.gz", "-v").
 				MatchErrContent("Error: plugin installation failed: file reached the 256 MiB size limit\n")
 		})
 	})
