b6828aa38887325e85a2ec53a6bc189b4eeaf458
Junjie Gao <junjiegao@microsoft.com>
2024-04-10 17:11:52 +0800

    test: improve e2e test
    Signed-off-by: Junjie Gao <junjiegao@microsoft.com>

--- test/e2e/suite/command/cert.go
+++ test/e2e/suite/command/cert.go
@@ -21,8 +21,8 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 )
 
-var _ = Describe("notation", func() {
-	It("show certs", func() {
+var _ = Describe("notation cert", func() {
+	It("show all", func() {
 		Host(BaseOptions(), func(notation *utils.ExecOpts, artifact *Artifact, vhost *utils.VirtualHost) {
 			notation.Exec("cert", "list").
 				MatchKeyWords(
@@ -31,11 +31,47 @@ var _ = Describe("notation", func() {
 		})
 	})
 
-	It("delete all certs", func() {
+	It("delete all", func() {
 		Host(BaseOptions(), func(notation *utils.ExecOpts, artifact *Artifact, vhost *utils.VirtualHost) {
 			notation.Exec("cert", "delete", "--all", "--type", "ca", "--store", "e2e", "-y").
 				MatchKeyWords(
 					"Successfully deleted",
+				)
+		})
+	})
+
+	It("delete a specfic cert", func() {
+		Host(BaseOptions(), func(notation *utils.ExecOpts, artifact *Artifact, vhost *utils.VirtualHost) {
+			notation.Exec("cert", "delete", "--type", "ca", "--store", "e2e", "e2e.crt", "-y").
+				MatchKeyWords(
+					"Successfully deleted e2e.crt",
+				)
+		})
+	})
+
+	It("delete a non-exist cert", func() {
+		Host(BaseOptions(), func(notation *utils.ExecOpts, artifact *Artifact, vhost *utils.VirtualHost) {
+			notation.ExpectFailure().Exec("cert", "delete", "--type", "ca", "--store", "e2e", "non-exist.crt", "-y").
+				MatchErrKeyWords(
+					"failed to delete the certificate file",
+				)
+		})
+	})
+
+	It("show e2e cert", func() {
+		Host(BaseOptions(), func(notation *utils.ExecOpts, artifact *Artifact, vhost *utils.VirtualHost) {
+			notation.Exec("cert", "list", "--type", "ca", "--store", "e2e").
+				MatchKeyWords(
+					"e2e.crt",
+				)
+		})
+	})
+
+	It("list", func() {
+		Host(BaseOptions(), func(notation *utils.ExecOpts, artifact *Artifact, vhost *utils.VirtualHost) {
+			notation.Exec("cert", "list").
+				MatchKeyWords(
+					"STORE TYPE   STORE NAME   CERTIFICATE",
 				)
 		})
 	})
