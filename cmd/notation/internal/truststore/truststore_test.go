734ea57ed8c5dcdae76731a08efa9cdb88b48671
Patrick Zheng <patrickzheng@microsoft.com>
2024-11-02 01:39:43 +0800

    test: add unit tests (#1075)
    This PR intends to add more unit tests to increase the code coverage.
    
    ---------
    
    Signed-off-by: Patrick Zheng <patrickzheng@microsoft.com>

--- cmd/notation/internal/truststore/truststore_test.go
+++ cmd/notation/internal/truststore/truststore_test.go
@@ -15,15 +15,107 @@ package truststore
 
 import (
 	"errors"
+	"os"
 	"path/filepath"
+	"runtime"
+	"strings"
 	"testing"
+
+	"github.com/notaryproject/notation-go/dir"
 )
 
-func TestEmptyCertFile(t *testing.T) {
-	path := filepath.FromSlash("../../../../internal/testdata/Empty.txt")
-	expectedErr := errors.New("no valid certificate found in the empty file")
-	err := AddCert(path, "ca", "test", false)
-	if err == nil || err.Error() != "no valid certificate found in the file" {
-		t.Fatalf("expected err: %v, got: %v", expectedErr, err)
-	}
+func TestAddCert(t *testing.T) {
+	defer func(oldDir string) {
+		dir.UserConfigDir = oldDir
+	}(dir.UserConfigDir)
+
+	t.Run("empty store type", func(t *testing.T) {
+		expectedErrMsg := "store type cannot be empty"
+		err := AddCert("", "", "test", false)
+		if err == nil || err.Error() != expectedErrMsg {
+			t.Fatalf("expected err: %v, but got: %v", expectedErrMsg, err)
+		}
+	})
+
+	t.Run("invalid store type", func(t *testing.T) {
+		expectedErrMsg := "unsupported store type: invalid"
+		err := AddCert("", "invalid", "test", false)
+		if err == nil || err.Error() != expectedErrMsg {
+			t.Fatalf("expected err: %v, but got: %v", expectedErrMsg, err)
+		}
+	})
+
+	t.Run("invalid store name", func(t *testing.T) {
+		expectedErrMsg := "named store name needs to follow [a-zA-Z0-9_.-]+ format"
+		err := AddCert("", "ca", "test%", false)
+		if err == nil || err.Error() != expectedErrMsg {
+			t.Fatalf("expected err: %v, but got: %v", expectedErrMsg, err)
+		}
+	})
+
+	t.Run("no valid certificate in file", func(t *testing.T) {
+		path := filepath.FromSlash("testdata/invalid.txt")
+		expectedErrMsg := "x509: malformed certificate"
+		err := AddCert(path, "ca", "test", false)
+		if err == nil || err.Error() != expectedErrMsg {
+			t.Fatalf("expected err: %v, but got: %v", expectedErrMsg, err)
+		}
+	})
+
+	t.Run("cert already exists", func(t *testing.T) {
+		dir.UserConfigDir = "testdata"
+		path := filepath.FromSlash("testdata/self-signed.crt")
+		expectedErrMsg := "certificate already exists in the Trust Store"
+		err := AddCert(path, "ca", "test", false)
+		if err == nil || err.Error() != expectedErrMsg {
+			t.Fatalf("expected err: %v, but got: %v", expectedErrMsg, err)
+		}
+	})
+
+	t.Run("empty file", func(t *testing.T) {
+		path := filepath.FromSlash("../../../../internal/testdata/Empty.txt")
+		expectedErr := errors.New("no valid certificate found in the empty file")
+		err := AddCert(path, "ca", "test", false)
+		if err == nil || err.Error() != "no valid certificate found in the file" {
+			t.Fatalf("expected err: %v, but got: %v", expectedErr, err)
+		}
+	})
+
+	t.Run("failed to add cert to store", func(t *testing.T) {
+		if runtime.GOOS == "windows" {
+			t.Skip("skipping test on Windows")
+		}
+
+		dir.UserConfigDir = t.TempDir()
+		if err := os.Chmod(dir.UserConfigDir, 0000); err != nil {
+			t.Fatal(err)
+		}
+		defer os.Chmod(dir.UserConfigDir, 0700)
+
+		path := filepath.FromSlash("testdata/NotationTestRoot.pem")
+		expectedErrMsg := "permission denied"
+		err := AddCert(path, "ca", "test", false)
+		if err == nil || !strings.Contains(err.Error(), expectedErrMsg) {
+			t.Fatalf("expected err: %v, but got: %v", expectedErrMsg, err)
+		}
+	})
+}
+
+func TestDeleteAllCerts(t *testing.T) {
+	defer func(oldDir string) {
+		dir.UserConfigDir = oldDir
+	}(dir.UserConfigDir)
+
+	t.Run("store does not exist", func(t *testing.T) {
+		if runtime.GOOS == "windows" {
+			t.Skip("skipping test on Windows")
+		}
+
+		dir.UserConfigDir = "testdata"
+		expectedErrMsg := `stat testdata/truststore/x509/tsa/test: no such file or directory`
+		err := DeleteAllCerts("tsa", "test", true)
+		if err == nil || err.Error() != expectedErrMsg {
+			t.Fatalf("expected err: %v, but got: %v", expectedErrMsg, err)
+		}
+	})
 }
