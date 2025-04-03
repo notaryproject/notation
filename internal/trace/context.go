6d7bb05fcc95003273b27b0bda7010a2e6f4ceef
Junjie Gao <junjiegao@microsoft.com>
2025-04-03 15:48:54 +0800

    fix: improve debug log
    Signed-off-by: Junjie Gao <junjiegao@microsoft.com>

--- internal/trace/context.go
+++ internal/trace/context.go
@@ -42,6 +42,8 @@ func WithLoggerLevel(ctx context.Context, level logrus.Level) context.Context {
 	var formatter logrus.TextFormatter
 	if level == logrus.DebugLevel {
 		formatter.FullTimestamp = true
+		// Set timestamp format to include nanoseconds and timezone
+		formatter.TimestampFormat = "2006-01-02 15:04:05.000000000 -07:00"
 	} else {
 		formatter.DisableTimestamp = true
 	}
