c5a9f5761466fe507999ed7a2ec099481d894966
Patrick Zheng <patrickzheng@microsoft.com>
2025-04-27 09:50:19 +0800

    fix e2e test
    Signed-off-by: Patrick Zheng <patrickzheng@microsoft.com>

--- internal/version/version.go
+++ internal/version/version.go
@@ -15,7 +15,7 @@ package version
 
 var (
 	// Version shows the current notation version, optionally with pre-release.
-	Version = "v1.3.2"
+	Version = "v1.3.1"
 
 	// BuildMetadata stores the build metadata.
 	//
